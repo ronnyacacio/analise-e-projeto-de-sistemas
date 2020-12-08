04888eac8901c6058ed0430b8c21bcfd48c9d405
ronnyacacio <ronnyacacio27@gmail.com>
2020-12-08 00:06:40 -0300

    final Maganer and SystemOperator

--- atividade02/src/br/com/ufc/model/person/Manager.java
+++ atividade02/src/br/com/ufc/model/person/Manager.java
@@ -3,6 +3,10 @@ package br.com.ufc.model.person;
 import br.com.ufc.model.product.Product;
 
 public class Manager extends Employee {
+
+  public Manager(String name, int registration) {
+    super(name, registration);
+  }
   
   public void addClient(Client client) {
     //
--- atividade02/src/br/com/ufc/model/person/SystemOperator.java
+++ atividade02/src/br/com/ufc/model/person/SystemOperator.java
@@ -1,11 +1,28 @@
 package br.com.ufc.model.person;
 
 public class SystemOperator extends Employee {
+
+  public SystemOperator(String name, int registration) {
+    super(name, registration);
+  }
+
   public void location() {
     //
   }
 
   public void removeLocation() {
+    //
+  }
+
+  public void devolution() {
+    //
+  }
+
+  public void findProductByCode() {
+    //
+  }
+
+  public void findClientByRegistration() {
     //
   }
 }
