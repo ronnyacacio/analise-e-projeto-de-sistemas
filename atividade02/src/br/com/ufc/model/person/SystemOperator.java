30de8326ad01f20bb910d60a2940d24884319404
ronnyacacio <ronnyacacio27@gmail.com>
2020-12-27 13:47:00 -0300

    remove methods in model SystemOperator

--- atividade02/src/br/com/ufc/model/person/SystemOperator.java
+++ atividade02/src/br/com/ufc/model/person/SystemOperator.java
@@ -5,24 +5,4 @@ public class SystemOperator extends Employee {
   public SystemOperator(String name, Integer registration) {
     super(name, registration);
   }
-
-  public void location() {
-    //
-  }
-
-  public void removeLocation() {
-    //
-  }
-
-  public void devolution() {
-    //
-  }
-
-  public void findProductByCode() {
-    //
-  }
-
-  public void findClientByRegistration() {
-    //
-  }
 }
